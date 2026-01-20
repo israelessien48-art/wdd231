5e075641e94ad8c1e5dacc63c8b043b0bbda211c
israelessien48-art <israelessien48@gmail.com>
2026-01-20 18:21:16 +0100

    update

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(window.location.search);
 
 ["fname", "lname", "email", "phone", "business", "timestamp"].forEach(field => {
-  document.getElementById(field).textContent = params.get(field);
+  document.getElementById(field).textContent = params.get(field) || "N/A";
 });
